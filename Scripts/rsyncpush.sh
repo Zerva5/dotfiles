d04ae135bb8351b97af883b95111685db5e3af25
Lucas Mayall <lucasmmayall@gmail.com>
2019-09-09 16:43:12 +0000

    Added vimwiki to rsync script

--- Scripts/rsyncpush.sh
+++ Scripts/rsyncpush.sh
@@ -3,6 +3,7 @@
 if [ $(hostname) == "LMDESK" ]
 then
 	rsync -a --delete --quiet -e ssh /home/lmayall/Documents/School/ lmayall@LMTHINK.local:/home/lmayall/Documents/School
+	rsync -a --delete --quiet -e ssh /home/lmayall/vimwiki/ lmayall@LMTHINK.local:/home/lmayall/vimwiki
 
 elif [ $(hostname) == "LMTHINK" ]
 then
