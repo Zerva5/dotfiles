0d3fe1236e92929c696623da7dfd46b550fea06e
Lucas Mayall <lucasmmayall@gmail.com>
2019-09-09 17:06:58 +0000

    Added other config folders to rsync script

--- Scripts/rsyncpush.sh
+++ Scripts/rsyncpush.sh
@@ -4,9 +4,18 @@ if [ $(hostname) == "LMDESK" ]
 then
 	rsync -a --delete --quiet -e ssh /home/lmayall/Documents/School/ lmayall@LMTHINK.local:/home/lmayall/Documents/School
 	rsync -a --delete --quiet -e ssh /home/lmayall/vimwiki/ lmayall@LMTHINK.local:/home/lmayall/vimwiki
+	rsync -a --delete --quiet -e ssh /home/lmayall/.config/nvim/ lmayall@LMTHINK.local:/home/lmayall/.config/nvim
+	rsync -a --delete --quiet -e ssh /home/lmayall/todo/ lmayall@LMTHINK.local:/home/lmayall/todo
+	
 
 elif [ $(hostname) == "LMTHINK" ]
 then
 	rsync -a --delete --quiet -e ssh /home/lmayall/Documents/School/ lmayall@LMDESK.local:/home/lmayall/Documents/School
+	rsync -a --delete --quiet -e ssh /home/lmayall/vimwiki/ lmayall@LMDESK.local:/home/lmayall/vimwiki
+	rsync -a --delete --quiet -e ssh /home/lmayall/.config/nvim/ lmayall@LMDESK.local:/home/lmayall/.config/nvim
+	rsync -a --delete --quiet -e ssh /home/lmayall/todo/ lmayall@LMDESK.local:/home/lmayall/todo
+	#rsync -a --delete --quiet -e ssh /home/lmayall/.config/nvim/ lmayall@LMDESK.local:/home/lmayall/.config/nvim
+	#rsync -a --delete --quiet -e ssh /home/lmayall/.config/nvim/ lmayall@LMDESK.local:/home/lmayall/.config/nvim
+
 
 fi
